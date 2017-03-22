60adf1409a061472e84b58b06c8c90ec10ae7a17
yuutaro-O <yuutarousoft@gmail.com>
2017-03-22 21:20:21 +0900

    記事、2ページ目の途中まで作成

---
+++ Assets/Scripts/CSVWriter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CSVWriter : MonoBehaviour {
+    public string fileName;
+	
+	//void Start () {
+ //       WriteCSV("Hello,World");
+	//}
+
+    public void WriteCSV(string txt)
+    {
+        StreamWriter streamWriter;
+        FileInfo fileInfo;
+        fileInfo = new FileInfo(Application.dataPath + "/" + fileName + ".csv");
+        streamWriter = fileInfo.AppendText();
+        streamWriter.WriteLine(txt);
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+}
---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+    float speed { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        speed = -0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.down * 10 * Time.deltaTime;
+        if(this.transform.position.y < -5.0f)
+        {
+            Debug.Log("false");
+            Destroy(this.gameObject);
+        }
+	}
+}
